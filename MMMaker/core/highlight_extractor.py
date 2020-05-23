a35b7eae9c22e23a30ab9da0be33d53f173606b1
AllyHyeseongKim <ally3k@icloud.com>
2020-05-23 17:17:26 +0900

    [highlight_extractor] Open the wave file in scipy

--- MMMaker/core/highlight_extractor.py
+++ MMMaker/core/highlight_extractor.py
@@ -1,16 +1,23 @@
 import wave
 from pydub import AudioSegment
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io.wavfile as wave
+from scipy.fftpack import fft
 
 
-def extract_highlights(file_paths):
+def extract_highlights():
     """
     :param file_paths: 동영상 경로 리스트
     :return: 추출한 하이라이트 동영상 경로 리스트
     """
 
-    source_video = AudioSegment.from_file('./SourceVideo.mp4', format="mp4")
-    source_video.export('./SourceWave.wav', format="wav")
+#    source_video = AudioSegment.from_file('/Users/hyeseongkim/Workspaces/Projects/MMMaker/SourceVideo.mp4', format="mp4")
+#    source_video.export('/Users/hyeseongkim/Workspaces/Projects/MMMaker/SourceWave.wav', format="wav")
 
-    source_wave = wave.open('./SourceWave.wav', 'r')
 
-    return file_paths
+    rate, source_wave = wave.read('/Users/hyeseongkim/Workspaces/Projects/MMMaker/SourceWave.wav')
+
+#    return file_paths
+
+extract_highlights()
