8599a41787c009bdb65e24460fdfee98b9ca5d0b
JungChaeMoon <jungchaemoon@gmail.com>
2020-05-23 21:45:23 +0900

    [video_merger] Return result file path

--- MMMaker/core/video_merger.py
+++ MMMaker/core/video_merger.py
@@ -7,12 +7,6 @@ def merge_videos(file_paths):
     clip_list = [VideoFileClip(file_path) for file_path in file_paths]
     final_clip = concatenate_videoclips(clip_list)
     final_clip.write_videofile('output.mp4')
-    f.close()
-
-    subprocess.run(['ffmpeg -f concat -safe 0 -i mylist.txt -c copy output.mp4'], shell=True, check=True)
-
-    os.remove('mylist.txt')
-    file_path = Path(file_paths[0]).parent
+    file_path = str(Path(file_paths[0]).parent) + '/' + 'output.mp4'
 
     return file_path
-
