4b6a3023ab2c57d463d4a7b834b08060d9b404d2
SimHongsub <ghdtjq2038@gmail.com>
2020-05-23 21:52:24 +0900

    [video_audio_manager] add comment to overwrite_audio function

--- MMMaker/core/video_audio_manager.py
+++ MMMaker/core/video_audio_manager.py
@@ -4,7 +4,7 @@ from moviepy.editor import *
 
 def extract_audio(video_path):
     """
-    :param video_path: 영상에 쓰일 하이라이트 영상 경로 리스트
+    :param video_path: 영상에 쓰일 하이라이트 영상 경로
     :return: 영상에서 추출된 오디오 파일 경로
     """
 
@@ -24,6 +24,16 @@ def extract_audio(video_path):
 
 
 def overwrite_audio(video_path, audio_path):
+    """
+    :param video_path: 영상에 쓰일 하이라이트 영상 경로
+    :param audio_path: 음계가 조정된 오디오 파일 경로
+    :return: 조정된 하이라이트 영상 경
+    """
+    # Access video file and audio file
+
+    # Adjust video speed to sync with audio file
+
+    # Overwrite video audio
 
     return video_path
 
