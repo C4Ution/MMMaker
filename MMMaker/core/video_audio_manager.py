2d77942a654801bd07f3f87b8fcc1eb55e064d46
SimHongsub <ghdtjq2038@gmail.com>
2020-05-23 20:58:37 +0900

    [video_audio_manager] Add module prototype

---
+++ MMMaker/core/video_audio_manager.py
@@ -0,0 +1,16 @@
+from moviepy.editor import *
+
+
+def extract_audio(video_path):
+    """
+    :param video_path: 영상에 쓰일 하이라이트 영상 경로 리스트
+    :return: 영상에서 추출된 오디오 파일 경로
+    """
+
+    return video_path
+
+
+def overwrite_audios(video_path, audio_path):
+
+    return video_path
+
