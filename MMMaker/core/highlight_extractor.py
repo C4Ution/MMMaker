64f5dd0655fd96cc1583a73e6643c30b027587b2
AllyHyeseongKim <ally3k@icloud.com>
2020-05-23 16:10:45 +0900

    [highlight_extractor] Extract the wave file from the source mp4

--- MMMaker/core/highlight_extractor.py
+++ MMMaker/core/highlight_extractor.py
@@ -1,3 +1,5 @@
+import wave
+from pydub import AudioSegment
 
 
 def extract_highlights(file_paths):
@@ -5,4 +7,10 @@ def extract_highlights(file_paths):
     :param file_paths: 동영상 경로 리스트
     :return: 추출한 하이라이트 동영상 경로 리스트
     """
+
+    source_video = AudioSegment.from_file('./SourceVideo.mp4', format="mp4")
+    source_video.export('./SourceWave.wav', format="wav")
+
+    source_wave = wave.open('./SourceWave.wav', 'r')
+
     return file_paths
