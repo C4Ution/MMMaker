290a0c6f21fdc3b1288464277a9c316eac9616f1
AllyHyeseongKim <ally3k@icloud.com>
2020-05-23 17:50:37 +0900

    [highlight_extractor] Calculate fourier transform of the wave file
    Use track[1]

--- MMMaker/core/highlight_extractor.py
+++ MMMaker/core/highlight_extractor.py
@@ -1,6 +1,5 @@
 import wave
 from pydub import AudioSegment
-import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wave
 from scipy.fftpack import fft
@@ -16,7 +15,13 @@ def extract_highlights():
 #    source_video.export('/Users/hyeseongkim/Workspaces/Projects/MMMaker/SourceWave.wav', format="wav")
 
 
-    rate, source_wave = wave.read('/Users/hyeseongkim/Workspaces/Projects/MMMaker/SourceWave.wav')
+    source_wave = wave.read('/Users/hyeseongkim/Workspaces/Projects/MMMaker/SourceWave.wav')
+
+    stereo_channel_wave = source_wave[1].T[1]
+    normalize_wave = [(ele / 2 ** 8.) * 2 - 1 for ele in stereo_channel_wave]  # this is 8-bit track, b is now normalized on [-1,1)
+    fourier_transform_wave = fft(normalize_wave)  # calculate fourier transform (complex numbers list)
+    plt.plot(fourier_transform_wave, 'r')
+    plt.show()
 
 #    return file_paths
 
