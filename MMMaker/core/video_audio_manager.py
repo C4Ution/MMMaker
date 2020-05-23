9534885dd6b2060bb934a704abd71a407e904876
SimHongsub <ghdtjq2038@gmail.com>
2020-05-23 22:37:45 +0900

    [video_audio_manager] overwrite video with adjusted audio

--- MMMaker/core/video_audio_manager.py
+++ MMMaker/core/video_audio_manager.py
@@ -11,7 +11,7 @@ def extract_audio(video_path):
     # Create random file name
     tf = tempfile.NamedTemporaryFile()
 
-    audio_path = os.getcwd() + "video_audio_manager_" + tf.name[4:] + ".wav"
+    audio_path = os.getcwd() + "/" + "video_audio_manager_" + tf.name[4:] + ".wav"
 
     # Access video file
     video_clip = VideoFileClip(video_path)
@@ -30,10 +30,20 @@ def overwrite_audio(video_path, audio_path):
     :return: 조정된 하이라이트 영상 경
     """
     # Access video file and audio file
+    video_clip = VideoFileClip(video_path)
+    audio_clip = AudioFileClip(audio_path)
+
+    # Create random file name
+    tf = tempfile.NamedTemporaryFile()
+
+    video_path = os.getcwd() + "/" + "video_audio_manager_" + tf.name[5:] + ".mp4"
 
     # Adjust video speed to sync with audio file
+    adjusted_video = video_clip.fx(vfx.speedx, final_duration=audio_clip.duration)
 
     # Overwrite video audio
+    adjusted_video.audio = audio_clip
+    adjusted_video.write_videofile(video_path)
 
     return video_path
 
