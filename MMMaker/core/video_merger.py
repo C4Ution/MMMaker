31f01aa2aafe3c24948520d749251e917ef81419
JungChaeMoon <jungchaemoon@gmail.com>
2020-05-23 17:12:28 +0900

    [video_merger] Write mp4 file_paths

--- MMMaker/core/video_merger.py
+++ MMMaker/core/video_merger.py
@@ -1,5 +1,11 @@
+import subprocess
+import os
 def merge_videos(file_paths):
-    """
+
+    f = open('mylist.txt', 'w')
+    for file_path in file_paths:
+        f.write('file \'{}\' \n'.format(file_path))
+    f.close()
 
     :param file_paths: 합쳐야할 영상 경로 리스트
     :return: 합쳐진 영상 경로
