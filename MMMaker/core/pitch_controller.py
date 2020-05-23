2d043eba474febd7d69db86af747f04f9d05ce40
myungsegyo <myungsekyo@gmail.com>
2020-05-23 21:15:09 +0900

    [pitch_controller] change module prototype

--- MMMaker/core/pitch_controller.py
+++ MMMaker/core/pitch_controller.py
@@ -3,14 +3,9 @@ from pydub import AudioSegment
 from pydub.playback import play
 
 
-def adjust_sounds(file_paths, music):
-    """
-    :param file_paths: 영상에 쓰일 하이라이트 영상 경로 리스트
-    :param music: 어떤 형식인지는 추후 상의
-    :return: 노래 길이에 맞춰 음계가 조정된 영상 경로 리스트
-    """
+def adjust_sounds(highlight_paths, silence_path, music):
 
-    return file_paths
+    return highlight_paths
 
 
 """
