bb6b58270694d31e4d9aa66d59537f931d6d0b45
AllyHyeseongKim <ally3k@icloud.com>
2020-05-23 21:09:08 +0900

    [highlight_extractor] Extract the highlight video from the source

--- MMMaker/core/highlight_extractor.py
+++ MMMaker/core/highlight_extractor.py
@@ -1,8 +1,7 @@
 import wave
 import numpy as np
 from moviepy.editor import *
-
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import scipy.io.wavfile as wave
 from scipy.fftpack import fft
 
@@ -14,10 +13,8 @@ def extract_highlights():
     :return: 추출한 하이라이트 동영상 경로 리스트
     """
     video_clip = VideoFileClip('/Users/hyeseongkim/Workspaces/Projects/MMMaker/SourceVideo.mp4')
-    source_video = video_clip.audio
-    source_video.write_audiofile('/Users/hyeseongkim/Workspaces/Projects/MMMaker/SourceWave.wav')
-
-
+    audio_clip = video_clip.audio
+    audio_clip.write_audiofile('/Users/hyeseongkim/Workspaces/Projects/MMMaker/SourceWave.wav')
     source_wave = wave.read('/Users/hyeseongkim/Workspaces/Projects/MMMaker/SourceWave.wav')
 
     stereo_channel_wave = source_wave[1].T[1]
@@ -26,7 +23,16 @@ def extract_highlights():
 #    plt.plot(fourier_transform_wave, 'r')
 #    plt.show()
 
+    normalize_time = len(fourier_transform_wave) / video_clip.duration
 
+    argmax_frequency = np.argmax(fourier_transform_wave) / normalize_time
+    argmin_frequency = np.argmin(fourier_transform_wave) / normalize_time
+
+    max_highlight = video_clip.subclip(argmax_frequency - 0.5, argmax_frequency + 0.5)
+    min_highlight = video_clip.subclip(argmin_frequency - 0.5, argmin_frequency + 0.5)
+
+#    max_highlight.write_videofile("/Users/hyeseongkim/Workspaces/Projects/MMMaker/MaxHighlight.mp4")
+#    min_highlight.write_videofile("/Users/hyeseongkim/Workspaces/Projects/MMMaker/MaxHighlight.mp4")
 #    return file_paths
 
 extract_highlights()
