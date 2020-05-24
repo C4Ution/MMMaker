42fad3633527f876ae3b8fe642124492ae193089
myungsegyo <myungsekyo@gmail.com>
2020-05-24 12:08:02 +0900

    [pitch_controller] migrate librosa from pydub

--- MMMaker/core/pitch_controller.py
+++ MMMaker/core/pitch_controller.py
@@ -1,9 +1,9 @@
-import pitch
-from pydub import AudioSegment
-from pydub.playback import play
+from .video_audio_manager import extract_audio, overwrite_audios
+import random
 
+from misc import random_str
 
-def adjust_sounds(highlight_paths, silence_path, music):
+HIGHLIGHT_LENGTH = 0.7
 
 # 미레도레 미미미 레레레 미솔솔 미레도레 미미미 레레미레도
 EXAMPLE_MUSIC = [
@@ -69,52 +69,72 @@ EXAMPLE_MUSIC = [
     },
 ]
 
+def adjust_sounds(highlight_paths, silence_path, music):
+    """
+    :param highlight_paths: 하이라이트 영상 경로 리스트
+    :param silence_path: 빈 소리 영상 경로
+    :param music:
+        example)
+            [
+                {
+                    'pitch': 4.5,
+                    'length': 0.7,
+                    'is_silence': False,
+                },
+                ...
+            ]
+    :return: 음계가 조정된 하이라이트 영상 경로 리스트
+    도 -9
+    도# -8
+    레 -7
+    레# -6
+    미 -5
+    파 -4
+    파# -3
+    솔 -2
+    솔# -1
+    라 0
+    라# 1
+    시 2
+    """
+    videos = []
+    audios = []
 
-"""
+    for note in music:
+        video = silence_path if note['is_silence'] else random.choice(highlight_paths)
+        videos.append(video)
+        wav = extract_audio(video)
 
-ffmpeg -i dog_barking_10sec.mp4 dog_barking.wav
-"""
-#
-sound = AudioSegment.from_file('dog.wav', format="wav")
-
-do = sound._spawn(sound.raw_data, overrides={'frame_rate': 26100})
-do_shop = sound._spawn(sound.raw_data, overrides={'frame_rate': 27700})
-rae = sound._spawn(sound.raw_data, overrides={'frame_rate': 29300})
-rae_shop = sound._spawn(sound.raw_data, overrides={'frame_rate': 31100})
-mi = sound._spawn(sound.raw_data, overrides={'frame_rate': 32900})
-pa = sound._spawn(sound.raw_data, overrides={'frame_rate': 34900})
-pa_shop = sound._spawn(sound.raw_data, overrides={'frame_rate': 36900})
-sol = sound._spawn(sound.raw_data, overrides={'frame_rate': 39100})
-sol_shop = sound._spawn(sound.raw_data, overrides={'frame_rate': 41500})
-la = sound._spawn(sound.raw_data, overrides={'frame_rate': 44000})
-la_shop = sound._spawn(sound.raw_data, overrides={'frame_rate': 46600})
-si = sound._spawn(sound.raw_data, overrides={'frame_rate': 49300})
-#
-#
-# play(sound._spawn(sound.raw_data, overrides={'frame_rate': 16000}).set_frame_rate(49300))
-# play(sound._spawn(sound.raw_data, overrides={'frame_rate': 16000}))
-# play(sound)
+    return highlight_paths
 
 
-# 미레도레미미미 레레레 미솔솔 미레도레 미미미 레레미레도
-play(mi)
-play(rae)
-play(do)
-play(rae)
-play(mi)
-play(mi)
-play(mi)
-play(rae)
-play(rae)
-play(rae)
-play(mi)
-play(sol)
-play(sol)
+def edit_sounds(path, pitch, length):
+    sr = 44100
+    output_path = '{}.wav'.format(random_str())
+    speed = HIGHLIGHT_LENGTH / length
+    y, sr = librosa.load(path, sr=sr)
+    write_wav(output_path, time_stretch(pitch_shift(y, sr, pitch), speed), sr)
+    return output_path
 
-import librosa
 
-sr = 44100
-y, sr = librosa.load('dog.wav', sr=sr)
-y_shifted = librosa.effects.pitch_shift(y, sr, n_steps=4.0, bins_per_octave=24)
-librosa.output.write_wav('dog_length.wav', librosa.effects.time_stretch(y_shifted, 2), sr=sr)
-librosa.output.write_wav('dog2.wav', y_shifted, sr=sr)
+if __name__ == '__main__':
+    import librosa
+    from librosa.output import write_wav
+    from librosa.effects import pitch_shift, time_stretch
+
+    sr = 44100
+    y, sr = librosa.load('dog.wav', sr=sr)
+    write_wav('C.wav', pitch_shift(y, sr, -9.0), sr)
+    write_wav('C#.wav', pitch_shift(y, sr, -8.0), sr)
+    write_wav('D.wav', pitch_shift(y, sr, -7.0), sr)
+    write_wav('D#.wav', pitch_shift(y, sr, -6.0), sr)
+    write_wav('E.wav', pitch_shift(y, sr, -5.0), sr)
+    write_wav('F.wav', pitch_shift(y, sr, -4.0), sr)
+    write_wav('F#.wav', pitch_shift(y, sr, -3.0), sr)
+    write_wav('G.wav', pitch_shift(y, sr, -2.0), sr)
+    write_wav('G#.wav', pitch_shift(y, sr, -1.0), sr)
+    write_wav('A.wav', pitch_shift(y, sr, 0.0), sr)
+    write_wav('A#.wav', pitch_shift(y, sr, 1.0), sr)
+    write_wav('B.wav', pitch_shift(y, sr, 2.0), sr)
+
+    write_wav('dog_length.wav', time_stretch(y, 2), sr=sr)
