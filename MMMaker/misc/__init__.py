3f20796db11fad7298fc5b15a5503f60bd6f27eb
myungsegyo <myungsekyo@gmail.com>
2020-06-03 14:11:49 +0900

    [misc] add get_random_name func

--- MMMaker/misc/__init__.py
+++ MMMaker/misc/__init__.py
@@ -1,5 +1,14 @@
+import os
 import secrets
+
+from conf.settings import BASE_DIR
+
+WORKING_DIR = os.path.join(BASE_DIR, 'temp_files')
 
 
 def random_str():
     return secrets.token_hex(nbytes=16)
+
+
+def get_random_name(extension):
+    return os.path.join(WORKING_DIR, '{}.{}'.format(random_str(), extension))
