9c63c836e7381cdc3c66e92b6f0d6662e5fa4637
myungsegyo <myungsekyo@gmail.com>
2020-06-16 21:26:18 +0900

    [web] add static settings

--- MMMaker/conf/urls.py
+++ MMMaker/conf/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from conf.views import IndexView
+
 urlpatterns = [
+    path('', IndexView.as_view()),
     path('admin/', admin.site.urls),
 ]
