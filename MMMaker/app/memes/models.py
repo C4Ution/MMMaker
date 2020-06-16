a49f2b2e0aab5a03fe71d5ccfa59f3a991823916
JungChaeMoon <jungchaemoon@gmail.com>
2020-06-16 22:50:19 +0900

    [meme] add meme model status

--- MMMaker/app/memes/models.py
+++ MMMaker/app/memes/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 
 class Task(models.Model):
-    STATUS_QUEUED, STATUS_STARTED, STATUS_DOWNLOAD, STATUS_FAILED = 10, 20, 30, 0
+    STATUS_QUEUED, STATUS_STARTED, STATUS_DOWNLOAD, STATUS_EXTRACT_HIGHLIGHT, STATUS_ADJUST_SOUNDS, STATUS_APPLY_EFFECTS, \
+    STATUS_MERGE_VIDEOS, STATUS_UPLOADER, STATUS_COMPLETE, STATUS_FAILED = 10, 20, 30, 40, 50, 60, 70, 80, 90, 0
+
     STATUS_CHOICES = (
         (STATUS_QUEUED, '서버 작업 대기중'),
         (STATUS_STARTED, '서버 작업 시작'),
         (STATUS_DOWNLOAD, '서버 리소스 다운로드'),
+        (STATUS_EXTRACT_HIGHLIGHT, '영상 하이라이트 추출'),
+        (STATUS_ADJUST_SOUNDS, '음계 조정'),
+        (STATUS_APPLY_EFFECTS, '영상 효과 추가'),
+        (STATUS_MERGE_VIDEOS, '영상 합성'),
+        (STATUS_UPLOADER, '영상 업로드'),
+        (STATUS_COMPLETE, '업로드 완료'),
         (STATUS_FAILED, '작업 실패'),
     )
     status = models.IntegerField(choices=STATUS_CHOICES, default=STATUS_QUEUED)
