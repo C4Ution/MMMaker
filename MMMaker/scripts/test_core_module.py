3ca02009adcb46e3f819e74bd9a0d3bf9b4a17fe
SimHongsub <ghdtjq2038@gmail.com>
2020-06-16 15:42:13 +0900

    [core] modify core test script by using music manager

--- MMMaker/scripts/test_core_module.py
+++ MMMaker/scripts/test_core_module.py
@@ -1,4 +1,5 @@
-from core.pitch_controller import adjust_sounds, EXAMPLE_MUSIC
+from core.music_manager import get_random_music
+from core.pitch_controller import adjust_sounds
 from core.video_merger import merge_videos
 from core.highlight_extractor import extract_highlights
 from core.video_effect_manager import apply_effects
@@ -6,6 +7,6 @@ from core.video_effect_manager import apply_effects
 
 def run():
     max_highlights, min_highlights = extract_highlights(['/home/simhongsub/dev/workspace/MMMaker/MMMaker/core/SourceVideo.mp4'])
-    videos = adjust_sounds(max_highlights, min_highlights, EXAMPLE_MUSIC)
+    videos = adjust_sounds(max_highlights, min_highlights, get_random_music())
     videos = apply_effects(videos)
     print(merge_videos(videos))
