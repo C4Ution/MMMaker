c74c31d9ed9c2c9d3368724f289a36904fa088f6
SimHongsub <ghdtjq2038@gmail.com>
2020-06-16 15:40:51 +0900

    [music_manager] add module to manage music

---
+++ MMMaker/core/music_manager.py
@@ -0,0 +1,312 @@
+import random
+
+# 떳다떳다 비행기
+# 미레도레 미미미 레레레 미솔솔 미레도레 미미미 레레미레도
+AIRPLANE_MUSIC = [
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -9.0,  # 도
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': 0.0,  # 공백
+        'length': 0.15,
+        'is_silence': True,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': 0.0,  # 공백
+        'length': 0.15,
+        'is_silence': True,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -2.0,  # 솔
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -2.0,  # 솔
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': 0.0,  # 공백
+        'length': 0.15,
+        'is_silence': True,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -9.0,  # 도
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': 0.0,  # 공백
+        'length': 0.15,
+        'is_silence': True,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -9.0,  # 도
+        'length': 0.1,
+        'is_silence': False,
+    },
+]
+
+# 아기상어
+# 레미 솔솔 솔솔솔솔솔 레미솔 솔솔솔솔솔 레미솔 솔솔솔솔솔 솔솔 파파
+BABY_SHARK_MUSIC = [
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -9.0,  # 도
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': 0.0,  # 공백
+        'length': 0.15,
+        'is_silence': True,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': 0.0,  # 공백
+        'length': 0.15,
+        'is_silence': True,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -2.0,  # 솔
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -2.0,  # 솔
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': 0.0,  # 공백
+        'length': 0.15,
+        'is_silence': True,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -9.0,  # 도
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': 0.0,  # 공백
+        'length': 0.15,
+        'is_silence': True,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -5.0,  # 미
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -7.0,  # 레
+        'length': 0.1,
+        'is_silence': False,
+    },
+    {
+        'pitch': -9.0,  # 도
+        'length': 0.1,
+        'is_silence': False,
+    },
+]
+
+
+def get_random_music():
+    musics = [
+        AIRPLANE_MUSIC,
+    ]
+
+    music_num = random.randint(0, len(musics)-1)
+
+    return musics[music_num]
+
