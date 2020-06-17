19c70bbafb3a4721e258971df0fcc0903dc60ec1
JungChaeMoon <jungchaemoon@gmail.com>
2020-06-18 07:53:11 +0900

    [video_effects] modify video_effects interface

--- MMMaker/core/video_effect_manager.py
+++ MMMaker/core/video_effect_manager.py
@@ -8,41 +8,48 @@ from misc import WORKING_DIR, get_random_name
 
 config.MELT_BINARY = '/usr/bin/melt'
 
-def _stay(video):
+
+def _stay(file_path):
+    video = VideoFileClip(file_path)
     file_path = get_random_name('mp4')
     video.write_videofile(file_path, codec='libx264', audio_codec='aac')
     return file_path
 
 
-def _flip_x(video):
+def _flip_x(file_path):
+    video = VideoFileClip(file_path)
     out = video.fx(vfx.mirror_x)
     file_path = get_random_name('mp4')
     out.write_videofile(file_path, codec='libx264', audio_codec='aac')
     return file_path
 
 
-def _flip_y(video):
+def _flip_y(file_path):
+    video = VideoFileClip(file_path)
     out = video.fx(vfx.mirror_y)
     file_path = get_random_name('mp4')
     out.write_videofile(file_path, codec='libx264', audio_codec='aac')
     return file_path
 
 
-def _black_white(video):
+def _black_white(file_path):
+    video = VideoFileClip(file_path)
     out = video.fx(vfx.blackwhite)
     file_path = get_random_name('mp4')
     out.write_videofile(file_path)
     return file_path
 
 
-def _colorx(video):
+def _colorx(file_path):
+    video = VideoFileClip(file_path)
     out = video.fx(vfx.colorx, factor=2)
     file_path = get_random_name('mp4')
     out.write_videofile(file_path, codec='libx264', audio_codec='aac')
     return file_path
 
 
-def _mask_color(video):
+def _mask_color(file_path):
+    video = VideoFileClip(file_path)
     out = video.fx(vfx.mask_color, color=[255, 0, 0], thr=100, s=5)
     # out = video.on_color(color=(255, 255, 0))
     file_path = get_random_name('mp4')
@@ -50,27 +57,32 @@ def _mask_color(video):
     return file_path
 
 
-def _rotate_90(video):
+def _rotate_90(file_path):
+    video = VideoFileClip(file_path)
     out = video.fx(vfx.rotate, angle=90)
     file_path = get_random_name('mp4')
     out.write_videofile(file_path, codec='libx264', audio_codec='aac')
     return file_path
 
 
-def _rotate_270(video):
+def _rotate_270(file_path):
+    video = VideoFileClip(file_path)
     out = video.fx(vfx.rotate, angle=270)
     file_path = get_random_name('mp4')
     out.write_videofile(file_path, codec='libx264', audio_codec='aac')
     return file_path
 
 
-def _invert_colors(video):
+def _invert_colors(file_path):
+    video = VideoFileClip(file_path)
     out = video.fx(vfx.invert_colors)
     file_path = get_random_name('mp4')
     out.write_videofile(file_path, codec='libx264', audio_codec='aac')
     return file_path
 
-def _painting(video):
+
+def _painting(file_path):
+    video = VideoFileClip(file_path)
     out = video.fx(vfx.painting, saturation = 1.6, black = 0.006)
     file_path = get_random_name('mp4')
     out.write_videofile(file_path, codec='libx264', audio_codec='aac')
@@ -93,6 +105,6 @@ def apply_effects(file_paths):
 
     for file_path in file_paths:
         effects_num = random.randint(0, len(effects)-1)
-        new_file_paths.append(effects[effects_num](video=VideoFileClip(file_path)))
+        new_file_paths.append(effects[effects_num](file_path))
 
     return new_file_paths
